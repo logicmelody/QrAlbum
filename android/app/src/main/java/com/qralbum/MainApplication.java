187da14d020639b5af348d9db9d26159d56d0579
Danny Lin <danny.l@ljit.io>
2018-06-26 11:04:44 +0800

    Show image picker

--- android/app/src/main/java/com/qralbum/MainApplication.java
+++ android/app/src/main/java/com/qralbum/MainApplication.java
@@ -3,6 +3,8 @@ package com.qralbum;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.wix.RNCameraKit.RNCameraKitPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +24,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNCameraKitPackage()
       );
     }
 
--- app/app.js
+++ app/app.js
@@ -1,8 +1,53 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+import Button from './components/Button';
 
-const App = () => (
-	<Text>This is app</Text>
-);
+class App extends Component {
+	selectPhotoTapped() {
+		ImagePicker.launchImageLibrary(null, response => {
+			console.log('Response = ', response);
+
+			if (response.didCancel) {
+				console.log('User cancelled photo picker');
+			} else if (response.error) {
+				console.log('ImagePicker Error: ', response.error);
+			} else if (response.customButton) {
+				console.log(
+					'User tapped custom button: ',
+					response.customButton
+				);
+			} else {
+				// You can also display the image using data:
+				let source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+				console.log('Image data = ' + response.data);
+
+				this.setState({
+					avatarSource: source
+				});
+			}
+		});
+	}
+	render() {
+		return (
+			<View style={styles.containerStyle}>
+				<Button
+					onPress={() => this.selectPhotoTapped()}
+				>
+					Show Image Picker
+				</Button>;
+			</View>
+		);
+	}
+}
+
+const styles = {
+	containerStyle: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+};
 
 export default App;
